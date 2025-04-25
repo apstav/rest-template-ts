59ee0f1c2137953619e4478bd8fbf5910941cde4
apstav <apostolis.stav@gmail.com>
2025-04-25 14:47:01 +0300

    coords in faker

--- ts-express-template/src/utils/faker.ts
+++ ts-express-template/src/utils/faker.ts
@@ -1,14 +1,26 @@
-const locations = ['Living Room', 'Bedroom', 'Kitchen', 'Bathroom', 'Outside'];
+const athensLocations = [
+  { lat: 37.9838, long: 23.7275 },
+  { lat: 37.9755, long: 23.7348 },
+  { lat: 37.9667, long: 23.7283 },
+  { lat: 37.9912, long: 23.7430 },
+  { lat: 37.9550, long: 23.7030 }, 
+];
 
 const faker = {
-  thermometerData: () => ({
-    deviceId: `DEV-${Math.floor(1000 + Math.random() * 9000)}`,
-    temperature: parseFloat((Math.random() * 30 + 10).toFixed(1)), // 10-40°C
-    humidity: Math.random() > 0.2 ? parseFloat((Math.random() * 100).toFixed(1)) : null,
-    batteryLevel: parseFloat((Math.random() * 100).toFixed(1)), // 0-100%
-    location: locations[Math.floor(Math.random() * locations.length)],
-    recordedAt: new Date() // Always include recordedAt
-  }),
+  thermometerData: () => {
+
+    const location = athensLocations[Math.floor(Math.random() * athensLocations.length)];
+
+    return {
+      deviceId: `DEV-${Math.floor(1000 + Math.random() * 9000)}`,
+      temperature: parseFloat((Math.random() * 30 + 10).toFixed(1)), // 10-40°C
+      humidity: Math.random() > 0.2 ? parseFloat((Math.random() * 100).toFixed(1)) : null,
+      batteryLevel: parseFloat((Math.random() * 100).toFixed(1)), // 0-100%
+      lat: location.lat,
+      long: location.long,
+      recordedAt: new Date()
+    };
+  }
 };
 
 export { faker };
